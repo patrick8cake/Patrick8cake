5f1fd58f028104f76de1d0b9d69676ac37e92ee5
patrick8cake <79605413+patrick8cake@users.noreply.github.com>
2021-06-05 11:47:42 -0300

    Update

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
-const navToggle = document.querySelector('.nav-toggle');
+// const navToggle = document.querySelector('.nav-toggle');
+// const links = document.querySelector('.links');
+
+// navToggle.addEventListener('click', function() {
+//     // if (links.classList.contains('show-links')) {
+//     //     links.classList.remove('show-links')
+//     // } else {
+//     //     links.classList.add('show-links');
+//     // }
+
+//     links.classList.toggle('show-links')
+// })
+
+//*************close Links*****************
+const toggleBtn = document.querySelector(".nav-toggle");
+const linkContainer = document.querySelector('.links-container');
 const links = document.querySelector('.links');
 
-navToggle.addEventListener('click', function() {
-    // if (links.classList.contains('show-links')) {
-    //     links.classList.remove('show-links')
-    // } else {
-    //     links.classList.add('show-links');
-    // }
+toggleBtn.addEventListener('click', function() {
+    const containerheight = linkContainer.getBoundingClientRect().height;
+    const linkHeight = links.getBoundingClientRect().height;
 
-    links.classList.toggle('show-links')
+    if (containerheight === 0) {
+        linkContainer.style.height = `${linkHeight}px`;
+    } else {
+        linkContainer.style.height = 0;
+    }
+
 })
+
+// *********** Fixed Nav bar *************
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link')
+
 
 window.addEventListener('scroll', () => {
     const navHeight = navbar.getBoundingClientRect().height;
@@ -22,8 +44,8 @@ window.addEventListener('scroll', () => {
     }
 
     if (scrollHeight > 500) {
-        topLink.classList.add('show-links');
+        topLink.classList.add('show-link');
     } else {
-        topLink.classList.remove('show-links');
+        topLink.classList.remove('show-link');
     }
 })
